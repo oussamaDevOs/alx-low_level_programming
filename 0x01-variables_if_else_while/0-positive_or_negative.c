0df93e360eef5728cf556b06223c159c71a802fa
Cherradi Oussama <cherradioussama1@gmail.com>
2023-03-17 17:25:46 -0700

    ALX

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,22 +1,23 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+ * return: always 0 (success)
+*/
+
 int main(void)
 {
+	int n;
 
-unsigned int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n < 0)
-	printf("%u is negative", n);
-else
-	printf("%u is positive", n);
-return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%u is positive", n);
+	else if (n == 0)
+		printf("%u is zero", n);
+	else
+		printf("%u is negative", n);
+	return (0);
 }
-
