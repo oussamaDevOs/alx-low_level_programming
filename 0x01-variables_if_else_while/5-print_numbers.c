97e342b3d7774a6b85250f54ef96aa46a682ec1c
Cherradi Oussama <cherradioussama1@gmail.com>
2023-03-19 09:33:29 -0700

    ALX

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+for (int r = 0; r < 10; r++)
+{
+putchar(r);
+}
+putchar('\n');
+
+return (0);
+}
