e0599b2b4141d7189494570eb7b41e81947d45e2
Cherradi Oussama <cherradioussama1@gmail.com>
2023-03-19 18:15:13 -0700

    ALX

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i, j, n = 10;
+
+for (i = 0; i < n - 1; i++)
+{
+for (j = i + 1; j < n; j++)
+{
+putchar(i + '0');
+putchar(j < n - 1 ? ',' : '\n');
+putchar(j < n - 1 ? ' ' : '\0');
+}
+}
+putchar('\n');
+return (0);
+}
+
