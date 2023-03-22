caa731c82db008d81afe539c9736c4d6c62ecfa4
Cherradi Oussama <cherradioussama1@gmail.com>
2023-03-22 15:41:02 -0700

    ALX

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void);
+{
+	for (int i = 0; i < 10; i++)
+{
+	for (char c = 'a'; c <= 'z'; c++)
+{
+	putchar(c);
+}
+	putchar('\n');
+}
+}
+
+	int main(void)
+{
+	print_alphabet_x10();
+
+	return (0);
+}
+
