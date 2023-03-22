bccacf2f9a1af6338a3d6d0086373132957f3b19
Cherradi Oussama <cherradioussama1@gmail.com>
2023-03-22 16:20:04 -0700

    ALX

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print alphabet_x10 - a function that will print the alphabet 10 times
+ * print_alphabet_x10 - a function that will print the alphabet 10 times
  *
  */
 
