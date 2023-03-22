fa99f5c829d17918f20398f5eac0f8446276ec3b
Cherradi Oussama <cherradioussama1@gmail.com>
2023-03-22 16:19:06 -0700

    ALX

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * main - Entry point
  * print alphabet_x10 - a function that will print the alphabet 10 times
- * Return: Always 0 (Success)
+ *
  */
 
-void print_alphabet_x10(void) 
+void print_alphabet_x10(void)
 {
 	int i, c;
 
