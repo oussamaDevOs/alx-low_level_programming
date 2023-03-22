e1d339fb6366f36185a3f88f329f46d264be31eb
Cherradi Oussama <cherradioussama1@gmail.com>
2023-03-22 16:05:50 -0700

    ALX

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,9 +6,10 @@
  * Return: Always 0 (Success)
  */
 
-void print_alphabet(void)
-	int i, c;
+void print_alphabet_x10(void)
 {
+	int i, c;
+
 	for (i = 0; i < 10; i++)
 {
 	for (c = 'a'; c <= 'z'; c++)
