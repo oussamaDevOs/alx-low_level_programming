70a6749d36e7cbaf8288ba7730d5d061bf9d463f
Cherradi Oussama <cherradioussama1@gmail.com>
2023-03-22 13:16:33 -0700

    ALX

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,13 +8,14 @@
 
 int main(void)
 {
-	char str[] = "_putchari\n";
+	char str[] = "_putchar";
 
 	int ch;
 
 	for (ch = 0; ch < 8; ch++)
 
 		_putchar(str[ch]);
+		_putchar('\n');
 
 	return (0);
 }
