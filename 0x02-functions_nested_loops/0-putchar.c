f1b93d5f8456ff2530f956f961a718242fdef725
Cherradi Oussama <cherradioussama1@gmail.com>
2023-03-21 17:16:37 -0700

    ALX

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
@@ -8,7 +8,11 @@
 
 int main(void)
 {
-printf("_putchar");
-return (0);
-}
+	char str[] = "_putchar"
+	int ch;
 
+	for(ch = 0; ch < 8; ch++);
+		_putchar(str[ch]);
+		_putchar('\n');
+	return(0);
+}
