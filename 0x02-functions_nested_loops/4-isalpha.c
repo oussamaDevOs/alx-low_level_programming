a0b42a4f1353a99c444ed431575ab680c4c4835e
Cherradi Oussama <cherradioussama1@gmail.com>
2023-03-22 19:08:19 -0700

    ALX

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * isalpha - detects alpha characters
+ * _isalpha - detects alpha characters
  *
  */
 
 int _isalpha(int c)
 {
 	if (c >= 97 && c <= 122)
-		return(1);
+		return (1);
 	else if (c >= 65 && c <= 90)
-		return(1);
+		return (1);
 	else
-		return(0);
+		return (0);
 }
