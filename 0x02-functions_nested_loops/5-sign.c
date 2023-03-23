d35e36270e1c685bc15aab07f99807fd32d7c0a1
Cherradi Oussama <cherradioussama1@gmail.com>
2023-03-22 19:43:38 -0700

    ALX

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,9 +2,9 @@
 
 /**
  *
- * _sign - finds and prints the sign of a given number
+ * print _sign - finds and prints the sign of a given number
  *
- * @n: number given 
+ * @n: number given
  *
  * return: return 1 if positvie return -& if negative otherwise always 0
  */
