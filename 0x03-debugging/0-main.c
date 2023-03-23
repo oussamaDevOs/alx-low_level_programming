77e611bf5d61acdbeef60b452e293c8f90c1990e
Cherradi Oussama <cherradioussama1@gmail.com>
2023-03-23 11:00:52 -0700

    ALX

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,25 +1,16 @@
 #include "main.h"
 
 /**
-* 
-* * main - tests function that prints if integer is positive or negative
+* main - tests function that prints if integer is positive or negative
 * Return: 0
-*
 */
 
 int main(void)
 {
-if (n > 0)
-{
-printf("%u is positive\n", n);
-}
-else if (n == 0)
-{
-printf("%u is zero\n", n);
-}
-else
-{
-printf("%u is negative\n", n);
-}
-return (0);
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
 }
