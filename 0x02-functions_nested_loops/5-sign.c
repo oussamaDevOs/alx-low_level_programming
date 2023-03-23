248f1873d63eb370352ad2b608f64103af318fda
Cherradi Oussama <cherradioussama1@gmail.com>
2023-03-22 19:45:54 -0700

    ALX

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print _sign - finds and prints the sign of a given number
+ * print_sign - finds and prints the sign of a given number
  *
  * @n: number given
  *
- * return: return 1 if positvie return -& if negative otherwise always 0
+ * Return: return 1 if positvie return -& if negative otherwise always 0
  */
 
 int print_sign(int n)
