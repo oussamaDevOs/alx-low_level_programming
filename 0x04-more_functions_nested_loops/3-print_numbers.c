c91ab5fe7a43929286a8ac056de9d2d294cf4a60
Cherradi Oussama <cherradioussama1@gmail.com>
2023-03-24 10:07:41 -0700

    ALX

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,4 @@
-#inlcude "main.h"
+#include "main.h"
 
 /**
  * print_numbers -  function that prints from 0 to 9
@@ -8,12 +8,12 @@
 
 void print_numbers(void)
 {
-	int digit == 0;
+	int digit = 0;
 
 	do {
-		_putchar(digit + 48);
-		digit;
-	}
-	while (digit >= 0 && digit <= 9);
-		_putchar('\n');
+		putchar(digit + 48);
+		digit++;
+	} while (digit >= 0 && digit <= 9);
+
+		putchar('\n');
 }
