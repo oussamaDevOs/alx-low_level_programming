a2b639130619cae6da133373c3e752f66c5302f2
Cherradi Oussama <cherradioussama1@gmail.com>
2023-03-24 14:29:10 -0700

    ALX

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - print _ n times
+ * @n: integer
+ */
+
+void print_line(int n)
+{
+	int line;
+
+	if (n <= 0)
+		putchar('\n');
+	else
+	{
+		for (line = 1; line <= n; line++)
+			putchar ('_');
+		putchar('\n');
+	}
+}
