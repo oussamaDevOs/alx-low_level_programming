a0c1c8e41634f7c809997ad37bea1f34a719c001
Cherradi Oussama <cherradioussama1@gmail.com>
2023-03-24 11:02:27 -0700

    ALX

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_numbers -  function that prints from 0 to 9
+ *
+ *
+ */
+
+void print_numbers(void)
+{
+	int digit;
+	int line;
+	int sym;
+
+	for (line = 0; line <= 10; line++)
+		for (digit = 0; digit  <= 14; digit++)
+			if (digit < 10)
+			{
+				putchar(digit + 48);
+			}
+			else
+			{
+				putchar(1);
+				putchar(digit + 48);
+			}
+			putchar('\n');
+}
