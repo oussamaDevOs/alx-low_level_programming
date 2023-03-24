d60cc05becd99af03faec9a9a455158fa65b6552
Cherradi Oussama <cherradioussama1@gmail.com>
2023-03-24 10:22:22 -0700

    ALX

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -13,7 +13,7 @@ void print_most_numbers(void)
 	do {
 		putchar(digit + 48);
 		digit++;
-	} while ((digit >= 0 && digit <= 9) && (digit != 50 && digit != 52));
+	} while ((digit >= 0 && digit <= 9) && (digit != 2 && digit != 4));
 
 		putchar('\n');
 }
