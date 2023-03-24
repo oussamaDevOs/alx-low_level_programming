4eff2a56264bbc9ac81c0bc6c03a2da84fad3d15
Cherradi Oussama <cherradioussama1@gmail.com>
2023-03-24 10:18:37 -0700

    ALX

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers -  function that prints from 0 to 9 except 2 and 4
+ *
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int digit = 0;
+
+	do {
+		putchar(digit + 48);
+		digit++;
+	} while ((digit >= 0 && digit <= 9) && (digit != 50 && digit != 52));
+
+		putchar('\n');
+}
