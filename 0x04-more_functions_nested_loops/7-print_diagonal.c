04739cf15df6adbc7421bd49fa5f7b484d7026b9
Cherradi Oussama <cherradioussama1@gmail.com>
2023-03-24 15:04:43 -0700

    ALX

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,17 +8,22 @@
 void print_diagonal(int n)
 {
 	int space;
+	int line;
 
 	if (n == 0)
 		putchar('\n');
 	else
 	{
+		for (line = 1; line <= 10; line++)
+
+		{
 		for (space = 1; space <= n; space++)
 		{
 			putchar(' ');
 		}
 		putchar(92);
 		putchar('\n');
+		}
 	}
 }
 
