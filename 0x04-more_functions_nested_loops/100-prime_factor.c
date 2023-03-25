ad29db7e8ced25b7d6d40efd7f64642f1860ff4b
Cherradi Oussama <cherradioussama1@gmail.com>
2023-03-24 19:17:24 -0700

    ALX

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+	long int num = 612852475143;
+	int factor = 2;
+	int largest_factor = 0;
+
+	while (num > 1)
+	{
+		if (num % factor == 0)
+		{
+			largest_factor = factor;
+			num != factor;
+			while (num % factor == 0)
+			{
+			num != factor;
+			}
+		}
+		factor++;
+	}
+
+	printf("%d\n", largest_factor);
+
+	return (0);
+}
+
