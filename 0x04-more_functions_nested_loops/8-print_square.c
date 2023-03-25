ed50b2003b30c770bc898e402f2fd63079023f05
Cherradi Oussama <cherradioussama1@gmail.com>
2023-03-24 18:53:58 -0700

    ALX

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,11 +10,15 @@ void print_square(int size)
 {
 	int row, column;
 
-	for (row = 1; row <= size; row++)
-	{
-		for (column = 1; column <= size; column++)
-			putchar('#');
+	if (size <= 0)
 		putchar('\n');
-	}
+	else
+
+		for (row = 1; row <= size; row++)
+		{
+			for (column = 1; column <= size; column++)
+				putchar('#');
+			putchar('\n');
+		}
 }
 
