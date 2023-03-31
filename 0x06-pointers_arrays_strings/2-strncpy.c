902478e5f23171b6b0ea46bfb63fe51edc009d96
Cherradi Oussama <cherradioussama1@gmail.com>
2023-03-30 19:49:21 -0700

    ALX

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,1 +1,22 @@
 #include "main.h"
+#include <string.h>
+
+/**
+ *
+ *
+ *
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+
+{
+	size_t i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	dest[i] = src[i];
+
+	for (; i < n; i++)	
+	dest[i] = '\0';
+
+	return (dest);
+}
