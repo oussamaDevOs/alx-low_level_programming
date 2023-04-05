c06cffe22889b4e2a8caa11b165a419610d92628
Cherradi Oussama <cherradioussama1@gmail.com>
2023-04-04 19:20:35 -0700

    ALX

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory
+ * @s: string
+ * @b: string
+ * @n: string
+ * Return: ptr
+ */
+
+char*_memset(char* s, char b, unsigned int n)
+
+{
+	char* ptr = s;
+
+	while (n--)
+	*s++ = b;
+	return (ptr);
+}
