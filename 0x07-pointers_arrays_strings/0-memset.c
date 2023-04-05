1b4167d53ea5218d686737679ff7ed223b94b103
Cherradi Oussama <cherradioussama1@gmail.com>
2023-04-04 19:42:12 -0700

    ALX

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,10 +8,10 @@
  * Return: ptr
  */
 
-char*_memset(char* s, char b, unsigned int n)
+char _memset(char *s, char b, unsigned int n)
 
 {
-	char* ptr = s;
+	char *ptr = s;
 
 	while (n--)
 	*s++ = b;
