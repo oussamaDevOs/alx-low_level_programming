e1f028f4d8ebe801c8da8d9da0723465ce52bfe2
Cherradi Oussama <cherradioussama1@gmail.com>
2023-04-05 19:45:46 -0700

    ALX

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -10,22 +10,10 @@
 int factorial(int n)
 {
 	if (n < 0)
-	{
 		return (-1);
-	}
 	else if (n == 0)
-	{
 		return (1);
-	}
 	else
-	{
-	int result = 1;
-	int i;
-
-	for (i = 1; i <= n; i++)
-	{
-		result *= i;
-	}
-		return (result);
-	}
+		return (n * factorial(n - 1));
 }
+
