f681f90e70bd0a1b44e5d229db625be56779d1c6
Cherradi Oussama <cherradioussama1@gmail.com>
2023-04-09 22:04:13 -0700

    ALX

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (; s[i] >= '\0', i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
+}
+
--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -2,8 +2,6 @@
 #define MAIN_H
 
 #include <stdio.h>
-
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -24,6 +22,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needl;
-
 
 #endif /* MAIN_H */
