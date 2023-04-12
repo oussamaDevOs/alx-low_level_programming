147aa9ca87d1ef8ed4dd164c8b414dc9ad6d741d
Cherradi Oussama <cherradioussama1@gmail.com>
2023-04-11 19:45:00 -0700

    ALX

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space
+ * @str: the string to duplicate
+ * Return: depends on conditions
+ */
+char *_strdup(char *str)
+{
+  char *dup_str;
+  unsigned int len = 0;
+  unsigned int i;
+
+  if (str == NULL)
+    return (NULL);
+
+  while (str[len] != '\0')
+    len++;
+
+  dup_str = malloc(sizeof(char) * (len + 1));
+
+  if (dup_str == NULL)
+    return (NULL);
+
+  for (i = 0; i <= len; i++)
+    dup_str[i] = str[i];
+
+  return (dup_str);
+}
+
