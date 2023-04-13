a88bf7b605d5ec57856481763c7c9541747445a8
Cherradi Oussama <cherradioussama1@gmail.com>
2023-04-12 19:53:05 -0700

    ALX

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,31 +9,32 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-  char *concat;
-  int len1 = 0, len2 = 0, i, j;
+	char *concat;
+	int len1 = 0, len2 = 0, i, j;
 
-  if (s1 == NULL)
-    s1 = "";
-  if (s2 == NULL)
-    s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-  while (s1[len1] != '\0')
-    len1++;
+	while (s1[len1] != '\0')
+		len1++;
 
-  while (s2[len2] != '\0')
-    len2++;
+	while (s2[len2] != '\0')
+		len2++;
 
-  concat = malloc((len1 + len2 + 1) * sizeof(char));
-  if (concat == NULL)
-    return (NULL);
+	concat = malloc((len1 + len2 + 1) * sizeof(char));
 
-  for (i = 0; s1[i] != '\0'; i++)
-    concat[i] = s1[i];
+	if (concat == NULL)
+		return (NULL);
 
-  for (j = 0; s2[j] != '\0'; j++)
-    concat[i + j] = s2[j];
+	for (i = 0; s1[i] != '\0'; i++)
+	concat[i] = s1[i];
 
-  concat[i + j] = '\0';
+	for (j = 0; s2[j] != '\0'; j++)
+		concat[i + j] = s2[j];
 
-  return (concat);
+	concat[i + j] = '\0';
+
+	return (concat);
 }
