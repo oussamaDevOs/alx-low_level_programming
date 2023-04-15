7cbc60a22aa284ae9e21940e20fdda949adb80ab
Cherradi Oussama <cherradioussama1@gmail.com>
2023-04-15 16:56:50 -0700

    ALX

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,8 +1,12 @@
-#include "main.h"
+#include <stdio.h>
 
-int main(int argc, char *argv[])
-{
-	printf("%d\n",argc - 1);
-	return (0);
+int count_arguments(int argc) {
+    return argc - 1;
+}
+
+int main(int argc, char *argv[]) {
+    int num_args = count_arguments(argc);
+    printf("%d\n", num_args);
+    return 0;
 }
 
