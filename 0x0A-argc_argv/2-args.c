b3f9b590511c967f687591d4724be35561225b4e
Cherradi Oussama <cherradioussama1@gmail.com>
2023-04-15 18:13:47 -0700

    ALX

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,11 +1,18 @@
 #include "main.h"
 
+/**
+ * main - entry point
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: always 0
+ */
+
 int main(int argc, char *argv[])
 {
 	int i;
-	
+
 	for (i = 0; i < argc; i++)
-		printf("%s\n",argv[i]);
+		printf("%s\n", argv[i]);
 	return (0);
 }
 
