34e11be1c17431aad18dd711296b888badbf903b
Cherradi Oussama <cherradioussama1@gmail.com>
2023-04-15 19:46:41 -0700

    ALX

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,8 +1,15 @@
 #include "main.h"
 
+/**
+ * main - Prints the number of arguments passed into the program
+ * @argc: The number of arguments
+ * @argv: vector
+ * Return: Always 0
+ */
+
 int main(int argc, char *argv[])
 {
-	printf("%d\n",argc - 1);
+	printf("%d\n", argc - 1);
 	return (0);
 }
 
