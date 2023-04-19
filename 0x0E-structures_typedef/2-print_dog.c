6110a6bd213b21a139de6ff15c0fdd7d724ad29e
Cherradi Oussama <cherradioussama1@gmail.com>
2023-04-18 19:55:18 -0700

    ALX

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -18,7 +18,5 @@ void print_dog(struct dog *d)
 	if (d->owner == NULL)
 		d->owner = "(Nil)";
 
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
