18fa0625f5375c65e7dabe3d8c70f8b471ddf246
Cherradi Oussama <cherradioussama1@gmail.com>
2023-04-18 19:52:59 -0700

    ALX

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -2,6 +2,13 @@
 #include <stdio.h>
 #include "dog.h"
 
+/**
+ * print_dog - prints a struct dog
+ * @d: pointer to struct dog
+ *
+ * Return: void
+ */
+
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
