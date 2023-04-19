1ea8d2c94527c0d39d76a8376453cb1a696a7882
Cherradi Oussama <cherradioussama1@gmail.com>
2023-04-18 19:42:25 -0700

    ALX

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,5 +7,7 @@ void print_dog(struct dog *d)
 	d->name = "Poppy";
 	d->age = atof("3.5");
 	d->owner = "Bob";
-	printf("Name: %s", d->name);
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
 }
