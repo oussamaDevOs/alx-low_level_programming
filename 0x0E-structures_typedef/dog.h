1a2d2bca726c8a530d39d0e4f8437df332d0b599
Cherradi Oussama <cherradioussama1@gmail.com>
2023-04-18 17:18:48 -0700

    ALX

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+
+/**
+ * struct dog - a struct representing a dog
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the dog's owner
+ */
+
 struct dog
 {
 	char *name;
