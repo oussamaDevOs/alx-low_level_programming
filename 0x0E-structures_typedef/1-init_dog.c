fccfd90550485ddf0c45d616b03e050b9a503bb5
Cherradi Oussama <cherradioussama1@gmail.com>
2023-04-18 19:17:03 -0700

    ALX

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include "dog.h"
 
+/**
+ * init_dog - initialize the previous function
+ * @d: dog structure
+ * @name: dog name
+ * @age: duh
+ * @owner: the one who let the dogs out
+ */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d->name = name;
