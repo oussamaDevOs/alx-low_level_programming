1930bd2a8974d6be2d4653cb1a4fea745292fa26
Cherradi Oussama <cherradioussama1@gmail.com>
2023-04-19 18:40:23 -0700

    ALX

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,9 +1,19 @@
 #include "function_pointers.h"
 #include <stdio.h>
 
+/**
+ * array_iterator - the function
+ * @array: The array
+ * @size: The size of the array.
+ * @action: A function pointer to the function
+ */
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	if (array == NULL || action == NULL)
+		return;
 	size_t i;
+
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
