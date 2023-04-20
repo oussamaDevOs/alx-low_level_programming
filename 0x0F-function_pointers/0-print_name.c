c0b40d1bbcb4520ba63345a210c19bbde70ecebe
Cherradi Oussama <cherradioussama1@gmail.com>
2023-04-19 17:33:51 -0700

    ALX

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,16 @@
 #include "function_pointers.h"
 #include <stdio.h>
 
+/**
+ * print_name - the function pointer
+ * @name:where we put the name
+ * @f: the pointer
+ */
+
 
 void print_name(char *name, void (*f)(char *))
 {
-	if(f == NULL || name == NULL)
+	if (f == NULL || name == NULL)
 		return;
-	else
-		f(name);
+	f(name);
 }
