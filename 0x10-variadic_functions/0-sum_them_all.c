6872309165da50f601767d8d4ee7ed496dedc9d9
Cherradi Oussama <cherradioussama1@gmail.com>
2023-04-21 15:52:56 -0700

    ALX

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,11 @@
 #include "variadic_functions.h"
-#include <stdlib.h>
+
+/**
+ * sum_them_all - returns the sum of all
+ * @n: the number of parameters
+ * @...: the parameters to sum
+ * Return: the sum of all
+ */
 
 int sum_them_all(const unsigned int n, ...)
 {
@@ -7,14 +13,15 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 
 	va_list args;
+
 	va_start(args, n);
-	
+
 	if (n == 0)
 		return (0);
 
 	for (i = 0; i < n; i++)
 		sum += va_arg(args, int);
-		
+
 	va_end(args);
 	return (sum);
 }
