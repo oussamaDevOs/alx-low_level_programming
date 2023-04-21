51ec3b0f31d306672cb5ce29401c444b13ef7598
Cherradi Oussama <cherradioussama1@gmail.com>
2023-04-20 19:37:57 -0700

    ALX

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,13 @@
+#include "3-calc.h"
+
+int main (void)
+{
+	int (*op_add)(int, int);
+	int (*op_sub)(int, int);
+	int (*op_div)(int, int);
+	int (*op_mul)(int, int);
+	int (*op_mod)(int, int);
+	int num1;
+	int num2;
+
+
