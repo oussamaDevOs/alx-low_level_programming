a5febfd35c95dcc9002eac2830edac38a09b6d75
Cherradi Oussama <cherradioussama1@gmail.com>
2023-04-21 16:24:25 -0700

    ALX

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	va_start(args, n);
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+	}
+	if (separator != NULL && i < n - 1)
+	{
+		printf("%s", separator);
+	}
+
+	va_end(args);
+
+	printf("\n");
+}
