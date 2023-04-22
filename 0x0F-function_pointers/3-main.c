0fa6a3d16fdab3508bb3a54f9d7c2cfaf7070082
Cherradi Oussama <cherradioussama1@gmail.com>
2023-04-21 19:35:12 -0700

    ALX

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,14 +1,47 @@
 #include "3-calc.h"
 
-int main (void)
-{
-	int (*op_add)(int, int);
-	int (*op_sub)(int, int);
-	int (*op_div)(int, int);
-	int (*op_mul)(int, int);
-	int (*op_mod)(int, int);
-	int num1;
-	int num2;
+/**
+ * main - Entry point
+ *
+ * @argc: arguments
+ * @argv: vectors
+ *
+ * Return: Always 0
+ */
 
-	return (0);
+int main(int argc, char *argv[])
+{
+    int num1, num2, result;
+    char *operator;
+    int (*op_func)(int, int);
+
+    if (argc != 4)
+    {
+        printf("Error\n");
+        return (98);
+    }
+
+    num1 = atoi(argv[1]);
+    operator = argv[2];
+    num2 = atoi(argv[3]);
+
+    op_func = get_op_func(operator);
+
+    if (op_func == NULL)
+    {
+        printf("Error\n");
+        return (99);
+    }
+
+    if ((operator[0] == '/' || operator[0] == '%') && num2 == 0)
+    {
+        printf("Error\n");
+        return (100);
+    }
+
+    result = op_func(num1, num2);
+    printf("%d\n", result);
+
+    return (0);
 }
+
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -5,7 +5,7 @@ int op_add(int a, int b)
 	return(a + b);
 }
 
-int op_add(int a, int b)
+int op_sub(int a, int b)
 {
 	return(a - b);
 }
