52658a48e5dc4cfd3c63ded74d574047f562ddc9
Cherradi Oussama <cherradioussama1@gmail.com>
2023-04-21 19:03:15 -0700

    ALX

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -3,10 +3,12 @@
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+	unsigned int i;
+
     va_list args;
     va_start(args, n);
 
-    for (unsigned int i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) {
         char *str = va_arg(args, char *);
         if (str == NULL) {
             printf("(nil)");
