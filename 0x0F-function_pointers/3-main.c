f924586bff773fd4b22848b5bd1cb87452133100
Cherradi Oussama <cherradioussama1@gmail.com>
2023-04-21 19:50:06 -0700

    ALX

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,37 +11,37 @@
 
 int main(int argc, char *argv[])
 {
-    int num1, num2, result;
-    char *operator;
-    int (*op_func)(int, int);
+	int num1, num2, result;
+	char *operator;
+	int (*op_func)(int, int);
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        return (98);
-    }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
 
-    num1 = atoi(argv[1]);
-    operator = argv[2];
-    num2 = atoi(argv[3]);
+	num1 = atoi(argv[1]);
+	operator = argv[2];
+	num2 = atoi(argv[3]);
 
-    op_func = get_op_func(operator);
+	op_func = get_op_func(operator);
 
-    if (op_func == NULL)
-    {
-        printf("Error\n");
-        return (99);
-    }
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		return (99);
+	}
 
-    if ((operator[0] == '/' || operator[0] == '%') && num2 == 0)
-    {
-        printf("Error\n");
-        return (100);
-    }
+	if ((operator[0] == '/' || operator[0] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
 
-    result = op_func(num1, num2);
-    printf("%d\n", result);
+	result = op_func(num1, num2);
+	printf("%d\n", result);
 
-    return (0);
+	return (0);
 }
 
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,26 +1,64 @@
 #include "3-calc.h"
 
+/**
+ * op_add - adding
+ * @a: integer
+ * @b: integer
+ * Return: result
+ */
+
 int op_add(int a, int b)
 {
-	return(a + b);
+	return (a + b);
 }
+
+/**
+ * op_sub - substracting
+ * @a: integer
+ * @b: integer
+ * Return: result
+ */
 
 int op_sub(int a, int b)
 {
-	return(a - b);
+	return (a - b);
 }
+
+/**
+ * op_mul - multiplicating
+ * @a: integer
+ * @b: integer
+ * Return: result
+ */
+
 
 int op_mul(int a, int b)
 {
-	return(a * b);
+	return (a * b);
 }
+
+/**
+ * op_div - dividing
+ * @a: integer
+ * @b: integer
+ * Return: result
+ */
+
 
 int op_div(int a, int b)
 {
-	return(a / b);
+	return (a / b);
 }
+
+/**
+ * op_mod - modulo
+ * @a: integer
+ * @b: integer
+ * Return: result
+ */
+
 
 int op_mod(int a, int b)
 {
-	return( a % b);
+	return (a % b);
 }
