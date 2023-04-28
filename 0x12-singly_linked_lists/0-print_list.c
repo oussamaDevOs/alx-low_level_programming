3eb0d4cb1201b04fc62f5dad4d0e295a9f507e9e
Oussama cherradi <cherradioussama1@gmail.com>
2023-04-28 03:56:47 -0700

    ALX

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,34 +6,34 @@
  * Return: depends
  */
 
-int _strlen(char *st)
+int _strlen(char *str)
 {
 	int i = 0;
 
-	if (!st)
+	if (!str)
 	{
 		return (0);
 	}
-	while (*st++)
+	while (*str++)
 		i++;
 	return (i);
 }
-
 
 /**
  * print_list - prints the elements of a list
  * @h: constant
  * Return: number of nodes
  */
+
 size_t print_list(const list_t *h)
 {
 	int i = 0;
 
 	while (h != NULL)
 	{
-	printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
-	i++;
-	h = h->next;
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+		i++;
+		h = h->next;
 	}
 	return (i);
 }
