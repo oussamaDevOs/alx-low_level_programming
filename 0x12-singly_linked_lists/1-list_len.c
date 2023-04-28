df7c95df60bcae691263cef0eb62d02114ac77bc
Oussama cherradi <cherradioussama1@gmail.com>
2023-04-28 04:14:16 -0700

    ALX

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * list_len - checks the number of elements
+ * @h: list
+ */
+
+size_t list_len(const list_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
