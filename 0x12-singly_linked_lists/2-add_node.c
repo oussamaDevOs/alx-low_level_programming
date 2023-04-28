865d773e1aa855b61890c98028a185c439bbabe7
Oussama cherradi <cherradioussama1@gmail.com>
2023-04-28 06:45:34 -0700

    ALX

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,15 +1,21 @@
 #include "lists.h"
 
+/**
+ * *add_node - function that adds a node at the beginning
+ * @head: first node
+ * @str: string
+ * Return: the new head
+ */
 
 
 list_t *add_node(list_t **head, const char *str)
 {
-	
+
 	list_t *add_node = malloc(sizeof(list_t));
 
 	if (add_node == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 
 	add_node->str = strdup(str);
@@ -17,12 +23,11 @@ list_t *add_node(list_t **head, const char *str)
 	if (add_node->str == NULL)
 	{
 		free(add_node);
-		return NULL;
+		return (NULL);
 	}
 
 
 	add_node->next = *head;
 	*head = add_node;
-    
 	return (add_node);
 }
