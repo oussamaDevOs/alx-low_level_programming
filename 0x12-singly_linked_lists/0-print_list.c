e99bd66046cd135f65f1fa2914bec2fe93725bab
Oussama cherradi <cherradioussama1@gmail.com>
2023-04-28 03:34:14 -0700

    ALX

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,11 +12,12 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-	printf("[%d] (%s)\n", h->len, h->str);
-	i++;
-	h = h->next;
-	}
 	if (h->str == NULL)
-		printf("[0] (nil)");
+               	printf("[0] (nil)");
+	else
+		printf("[%d] (%s)\n", h->len, h->str);
+		i++;
+		h = h->next;
+	}
 	return (i);
 }
